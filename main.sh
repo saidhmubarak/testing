96cc0a82db3bd194eb39426e426456b755e506b5
saidhmubarak <43137412+saidhmubarak@users.noreply.github.com>
2022-07-04 22:29:54 +0530

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,16 +1,25 @@
-#! /bin/bash
-pkg list-installed > satisfied.txt
+#! /bin/bash 
+#pkg list-installed >& satisfied.txt
 file=satisfied.txt
-match=$(grep "termux-api" satisfied.txt)
+match=$(grep "temux-api" satisfied.txt)
+match1=$(cat test.txt)
 main () {
-	while read line; do
-		if [[ $match == "termux-api" ]]; then
-			echo "its working "
-		else
-			echo "its not working"
-		fi
-	done < satisfied.txt
+	if [[ $match == "$match1" ]]; then
+		
+		
+	else
+		cat <<- _EOF_
+		[-] I don't find any dependencies
+		[+] Don't worry i will install it for you
+		[!] Please wait...
+		_EOF_
+		#pkg install termux-api >& /dev/null
+		echo "[+] Installation completed succesfully"
+		echo "[!] Please restart the script"
+		
+	fi
 }
+main
 		
 		
 
